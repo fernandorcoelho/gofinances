202063390b570f40bf4d4293bf8d20b5c0f30eba
Team <team@fastcode.com.br>
2022-04-18 17:25:42 -0300

    Using proportions in measurements

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,21 @@ import * as S from "./styles";
 export const Dashboard = () => {
   return (
     <S.Container>
-      <S.Title>Dashboard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/63662083?v=4",
+              }}
+            />
+            <S.User>
+              <S.UserGreeting>Olá,</S.UserGreeting>
+              <S.Username>Fernando</S.Username>
+            </S.User>
+          </S.UserInfo>
+        </S.UserWrapper>
+      </S.Header>
     </S.Container>
   );
 };
